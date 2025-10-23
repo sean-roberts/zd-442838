a2248d4269f2e6200e03ec3b5cc01abfa5510a6e
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-10-23 20:34:40 +0800

    added data

--- netlify/functions/run.ts
+++ netlify/functions/run.ts
@@ -2,7 +2,11 @@ import { AsyncWorkloadsClient } from '@netlify/async-workloads'
 import type { Config } from '@netlify/functions'
 
 export default async function() {
-  await new AsyncWorkloadsClient().send('workload')
+  await new AsyncWorkloadsClient().send('workload', {
+    data: {
+      sleepMessage: 'starting sleep sequence'
+    }
+  })
 
   return new Response(null, {
     status: 204
